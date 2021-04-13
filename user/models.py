6e2d97ea912c056f719b7d028307e7faa04d3436
Ayun Sim <shim52111@gmail.com>
2021-04-13 15:07:03 +0900

    Member로 바꿨어요(_ _ )

--- user/models.py
+++ user/models.py
@@ -6,9 +6,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class User(models.Model):
+class Member(models.Model):
     objects = models.Manager()
-    user_id = models.CharField(max_length=50)
+    user_id = models.CharField(max_length=50, primary_key=True)
     pw = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
@@ -16,7 +16,7 @@ class User(models.Model):
     nickname = models.CharField(max_length=50)
 
 
-class User_Profile(models.Model):
+class Member_Profile(models.Model):
     objects = models.Manager()
     profile_id = models.AutoField(primary_key=True)
     profile_info = models.TextField()
@@ -25,11 +25,11 @@ class User_Profile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_User(sender, instance, created, **kwargs):
+def create_user_Member(sender, instance, created, **kwargs):
     if created:
-        User.objects.create(user=instance, user_pk=instance.id)
+        Member.objects.create(user=instance, user_pk=instance.id)
 
 
 @receiver(post_save, sender=User)
-def save_user_User(sender, instance, **kwargs):
-    instance.User.save()
+def save_user_Member(sender, instance, **kwargs):
+    instance.Member.save()
