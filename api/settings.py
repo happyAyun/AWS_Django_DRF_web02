c471ec36c7c4be399f0ac7d858da300b630a3197
choi <chjcmy@gmail.com>
2021-04-26 19:17:10 +0900

    edit  dockerfile

--- api/settings.py
+++ api/settings.py
@@ -46,7 +46,7 @@ SECRET_KEY = 'xt!de@_*w0!y*@x!bqsb6%zr*tmhh+z2qjoaqwru4vq1@-d-fa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 APPEND_SLASH = False  # 추가 안해줄시 기본값이 True인데 그 경우 urls.py에서 경로설정시 주소 끝에 /를 붙이고
 # 해당경로로 /를 붙이지 않고 접속시 페이지를 찾을 수 없기때문에 리다이렉트를 시켜 자동으로 /를 붙여서 경로를 찾는다.
